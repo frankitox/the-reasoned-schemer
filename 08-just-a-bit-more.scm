2dad01d0bd37e46c406cc4b951f9641e3502b24c
franquito <franconbiasin@gmail.com>
2017-07-23 23:37:58 -0300

    starting chapter eight

---
+++ 08-just-a-bit-more.scm
@@ -0,0 +1,31 @@
+(load "mk.scm")
+(load "mkextraforms.scm")
+(load "07-a-bit-too-much.scm")
+
+(define *o
+  (lambda (n m p)
+    (condi
+      ((== () n) (== () p))
+      ((poso n) (== () m) (== () p))
+      ((== '(1) n) (poso m) (== m p))
+      ((>1o n) (== '(1) m) (== n p))
+      ((fresh (x z)
+         (== (cons 0 x) n) (poso x)
+         (== (cons 0 z) p) (poso z)
+         (>1o m)
+         (*o x m z)))
+      ((fresh (x y)
+         (== (cons 1 x) n) (poso x)
+         (== (cons 0 y) m) (poso y)
+         (*o m n p)))
+      ((fresh (x y)
+         (== (cons 1 x) n) (poso x)
+         (== (cons 1 y) m) (poso y)
+         (odd-*o x n m p))))))
+
+(define odd-*o
+  (lambda (x n m p)
+    (fresh (q)
+      (bound-*o p q n m)
+      (*o x m q)
+      (+o (cons 0 q) m p))))
